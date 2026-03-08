46013caa9ce2bd1e1f5d6c1c72aafde812c4c2fb
root <emadha785@gmail.com>
2026-03-08 11:08:26 +0300

    main.py updated

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ class Person():
         self.name = name
         self.age = age
 
-    def showInfo():
+    def showInfo(self):
         return f"Hello {self.name}"
+    
+    def age_in_days(self):
+        return f"Your age in days is {self.age * 365}"
 
 name = input("Enter your name: ")
-age = int(input("Enter your age: ")
-
+age = int(input("Enter your age: "))
 p1 = Person(name,age)
 
 print(p1.showInfo())
+
+print(p1.age_in_days())
